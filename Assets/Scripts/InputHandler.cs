af164ffca6da63a8e220c282d2df361425542e1f
Ramatoulaye Bah <141573185+RamatoulayeBah28@users.noreply.github.com>
2025-03-11 21:47:53 -0500

    all clues have exit buttons and work

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -20,43 +20,50 @@ public class InputHandler : MonoBehaviour
         Cursor.SetCursor(null, Vector2.zero, cursorMode);
     }
 
-    public void OnClick(InputAction.CallbackContext context) {
+    public void OnClick(InputAction.CallbackContext context)
+    {
         if (!context.started) return;
 
         var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
 
         // checking that what the ray hits is not null and it's the object with the tag "Book"
-        if (!rayHit.collider) {
+        if (!rayHit.collider)
+        {
 
             Debug.Log("No valid object clicked");
-            return;        
+            return;
         }
 
 
-        if (rayHit.collider.CompareTag("Book")) {
-             Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        if (rayHit.collider.CompareTag("Book"))
+        {
+            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
             // Successfully hit the game object
             Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
 
             // when clicked, go to openBook scene
             SceneController.instance.ChangeScene("BookScene");
-            
-        }else if(rayHit.collider.CompareTag("TV")){
-             Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
-            
+
+        }
+        else if (rayHit.collider.CompareTag("TV"))
+        {
+            Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
+
 
             // when clicked, go to TV scene
             SceneController.instance.ChangeScene("TVScene");
         }
 
-        else if(rayHit.collider.CompareTag("Painting")){
-             Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
-            
+        else if (rayHit.collider.CompareTag("Painting"))
+        {
+            Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
+
 
             // when clicked, go to Painting scene
             SceneController.instance.ChangeScene("PictureScene");
         }
-        else if (rayHit.collider.CompareTag("Drawer")) {
+        else if (rayHit.collider.CompareTag("Drawer"))
+        {
             Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
             // Successfully hit the game object
             Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
@@ -65,7 +72,8 @@ public class InputHandler : MonoBehaviour
             SceneController.instance.ChangeScene("DrawerScene");
         }
 
-        else if (rayHit.collider.CompareTag("Clock")) {
+        else if (rayHit.collider.CompareTag("Clock"))
+        {
             Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
             // Successfully hit the game object
             Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
@@ -75,27 +83,74 @@ public class InputHandler : MonoBehaviour
         }
 
         // x symbol when clicking on it, goes back to sample scene
-        else if (rayHit.collider.CompareTag("ExitBook")) {
+        else if (rayHit.collider.CompareTag("ExitBook"))
+        {
             Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
             // Successfully hit the game object
             Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
 
             // when clicked, go to openBook scene
-            SceneController.instance.ChangeScene("Scenes/RoomScene");
+            if (SceneController.instance != null)
+            {
+                SceneController.instance.ChangeScene("Scenes/RoomScene");
+            }
+            else
+            {
+                Debug.LogError("SceneController instance is null. Make sure it's added to the scene.");
+            }
         }
 
-        else if (rayHit.collider.CompareTag("ExitClock")) {
+        else if (rayHit.collider.CompareTag("ExitClock"))
+        {
             Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
             // Successfully hit the game object
             Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
 
-            // when clicked, go to openBook scene
-            SceneController.instance.ChangeScene("Scenes/RoomScene");
+            // when clicked, go back to room scene
+            if (SceneController.instance != null)
+            {
+                SceneController.instance.ChangeScene("Scenes/RoomScene");
+            }
+            else
+            {
+                Debug.LogError("SceneController instance is null. Make sure it's added to the scene.");
+            }
         }
 
-        
+        else if (rayHit.collider.CompareTag("ExitPainting"))
+        {
+            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+            // Successfully hit the game object
+            Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
 
-        
+            // when clicked, go back to room scene
+            if (SceneController.instance != null)
+            {
+                SceneController.instance.ChangeScene("Scenes/RoomScene");
+            }
+            else
+            {
+                Debug.LogError("SceneController instance is null. Make sure it's added to the scene.");
+            }
+        }
+
+        else if (rayHit.collider.CompareTag("ExitTV"))
+        {
+            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+            // Successfully hit the game object
+            Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
+
+            // when clicked, go back to room scene
+            if (SceneController.instance != null)
+            {
+                SceneController.instance.ChangeScene("Scenes/RoomScene");
+            }
+            else
+            {
+                Debug.LogError("SceneController instance is null. Make sure it's added to the scene.");
+            }
+        }
+
     }
 
 }
