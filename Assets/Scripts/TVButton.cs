a216e9a62ea6f68a838cc02fd5bc6edb9d595064
aabdelhai <meddy555@hotmail.com>
2025-03-26 15:04:26 -0500

    TV Remote beginning

---
+++ Assets/Scripts/TVButton.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TVButton : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void OnCLick()
+    {
+        
+    }
+}
