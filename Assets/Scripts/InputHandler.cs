ac4701b6d3e6db9159756d3f8fcbf20f72250ad3
BaSaBu1 <sambuu.coding@gmail.com>
2025-03-25 21:41:00 -0500

    Better InputHandler
    1. Solved redundancy in InputHandler
    2. PictureScene is now renamed PaintingScene

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -4,20 +4,10 @@ using UnityEngine.InputSystem;
 public class InputHandler : MonoBehaviour
 {
     private Camera _mainCamera;
-    // public Texture2D defaultTexture;
-    // public Texture2D examineTexture;
-    // public CursorMode curMode = CursorMode.Auto;
-    // public Vector2 hotSpot = Vector2.zero;
-    public Texture2D cursorTexture;
-    public CursorMode cursorMode = CursorMode.Auto;
-    public Vector2 hotSpot = Vector2.zero;
-    [SerializeField]
-    GameObject codePanel;
 
     private void Awake()
     {
         _mainCamera = Camera.main;
-        Cursor.SetCursor(null, Vector2.zero, cursorMode);
     }
 
     public void OnClick(InputAction.CallbackContext context)
@@ -26,68 +16,18 @@ public class InputHandler : MonoBehaviour
 
         var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
 
-        // checking that what the ray hits is not null and it's the object with the tag "Book"
+        // checking that what the ray hits is not null
         if (!rayHit.collider)
         {
-
             Debug.Log("No valid object clicked");
             return;
         }
 
-
-        if (rayHit.collider.CompareTag("Book"))
-        {
-            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
-            // Successfully hit the game object
-            Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
-
-            // when clicked, go to openBook scene
-            SceneController.instance.ChangeScene("BookScene");
-
-        }
-        else if (rayHit.collider.CompareTag("TV"))
-        {
-            Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
-
-
-            // when clicked, go to TV scene
-            SceneController.instance.ChangeScene("TVScene");
-        }
-
-        else if (rayHit.collider.CompareTag("Painting"))
-        {
-            Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
-
-
-            // when clicked, go to Painting scene
-            SceneController.instance.ChangeScene("PictureScene");
-        }
-        else if (rayHit.collider.CompareTag("Drawer"))
-        {
-            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
-            // Successfully hit the game object
-            Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
-
-            // when clicked, go to drawer scene
-            SceneController.instance.ChangeScene("DrawerScene");
-        }
-
-        else if (rayHit.collider.CompareTag("Clock"))
-        {
-            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
-            // Successfully hit the game object
-            Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
-
-            // when clicked, go to drawer scene
-            SceneController.instance.ChangeScene("ClockScene");
-        }
-
         // x symbol when clicking on it, goes back to sample scene
-        else if (rayHit.collider.CompareTag("Exit") || rayHit.collider.CompareTag("ExitTV"))
+        else if (rayHit.collider.CompareTag("Exit"))
         {
-            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
             // Successfully hit the game object
-            Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {rayHit.collider.tag}");
+            Debug.Log("Exiting");
 
             // when clicked, go to openBook scene
             if (SceneController.instance != null)
@@ -98,6 +38,13 @@ public class InputHandler : MonoBehaviour
             {
                 Debug.LogError("SceneController instance is null. Make sure it's added to the scene.");
             }
+        }
+        else {
+            string tag = rayHit.collider.tag.ToString();
+            Debug.Log($"Object Name: {rayHit.collider.gameObject.name}, Tag: {tag}");
+
+            string scene = tag + "Scene";
+            SceneController.instance.ChangeScene(scene);
         }
     }
 }
