a8b6e65a03d2888d5a99be18ccf9357dec9c46dd
aabdelhai <meddy555@hotmail.com>
2025-03-31 10:32:19 -0500

    Code Panel Complete Not changing scenes though

--- Assets/Scripts/ChannelSelector.cs
+++ Assets/Scripts/ChannelSelector.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChannelSelector : MonoBehaviour
 {
+     [SerializeField]
+    Text channelText;
+    string selectChannel = "";
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        channelText.text = selectChannel;
+        if(selectChannel == "17"){
+            SceneController.instance.ChangeScene("Scenes/TVChannelScene"); 
+        } 
+        if(selectChannel.Length >= 2){
+            selectChannel = "";
+        } 
+        Debug.Log($"Channel: {selectChannel}");
+    }
+    public void AddDigit(string digit) {
+        selectChannel += digit;
     }
 }
