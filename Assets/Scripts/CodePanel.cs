c87ca4d141e9ecbcf6b861999b13933448121424
Ramatoulaye Bah <141573185+RamatoulayeBah28@users.noreply.github.com>
2025-04-03 23:38:18 -0500

    improved the keypad

--- Assets/Scripts/CodePanel.cs
+++ Assets/Scripts/CodePanel.cs
@@ -2,30 +2,41 @@ using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 
 public class CodePanel : MonoBehaviour
 {
     private bool isUnlocked = false;
     [SerializeField]
-    Text codeText;
+    TextMeshProUGUI codeText;
     string codeTextValue = "";
-     void Start()
+    void Start()
     {
         gameObject.SetActive(true); 
     }
     void Update()
     {
-        codeText.text = codeTextValue;
-        if(!isUnlocked){
-            if (codeTextValue == "5510") {
+       
+        if (codeText != null)
+        {
+            // If codeTextValue is empty, show "Enter Code", otherwise, show the actual codeTextValue (the digits the player entered)
+            codeText.text = string.IsNullOrEmpty(codeTextValue) ? "Enter Code" : codeTextValue;
+        }
+
+        if (!isUnlocked)
+        {
+            if (codeTextValue == "5510")
+            {
+                codeText.color = Color.green;
                 AudioManager.instance.PlayUnlocked();
                 isUnlocked = true;
                 ClueManager.instance.isDrawerUnlocked = true;
                 SceneController.instance.ChangeScene("Scenes/OpenDrawerScene");
             }
 
-            if (codeTextValue.Length >= 4) {
+            if (codeTextValue.Length == 4)
+            {
                 codeTextValue = "";
             }
         }
@@ -33,5 +44,20 @@ public class CodePanel : MonoBehaviour
 
     public void AddDigit(string digit) {
         codeTextValue += digit;
+    }
+
+    public void PressClear()
+    {
+        codeTextValue = "";
+        codeText.text = "Enter Code";
+    }
+
+    public void PressDelete()
+    {
+        if (codeTextValue.Length > 0)
+        {
+            codeTextValue = codeTextValue.Substring(0, codeTextValue.Length - 1);
+            
+        }
     }
 }
