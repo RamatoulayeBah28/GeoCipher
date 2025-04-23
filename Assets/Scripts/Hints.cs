4a5559421fe7b9ed75d5f9d40df0d47fa5a76d67
BaSaBu1 <sambuu.coding@gmail.com>
2025-04-23 00:04:43 -0500

    Small updates
    - Improved hint logic
    - Added negative feedback sound for the clock
    - Balanced all sound volume levels

--- Assets/Scripts/Hints.cs
+++ Assets/Scripts/Hints.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using TMPro;
 
@@ -12,6 +13,8 @@ public class Hints : MonoBehaviour
 
     private List<string> hints = new List<string>()
     {
+        "",
+
         "You might want to start by reading",
         "Books have more than just stories",
         "Numbers are everywhere. They could be telling you something",
@@ -41,35 +44,74 @@ public class Hints : MonoBehaviour
         if (currentHintIndex < hints.Count)
         {
             if(hintsUsed < allowedHints){
-                if(ClueManager.instance.isBookUnlocked){
+                if(ClueManager.instance.isBookUnlocked && currentHintIndex < 5){
                     currentHintIndex = 5;
+                } else {
+                    if(!ClueManager.instance.isBookUnlocked && currentHintIndex > 4){
+                        currentHintIndex = 4;
+                        hintsUsed--;
+                    }
                 }
 
-                if(ClueManager.instance.isTVFound){
+                if(ClueManager.instance.isTVFound && currentHintIndex < 7){
                     currentHintIndex = 7;
+                } else {
+                    if(!ClueManager.instance.isTVFound && currentHintIndex > 6){
+                        currentHintIndex = 6;
+                        hintsUsed--;
+                    }
                 }
 
-                if(ClueManager.instance.isPaintingUnlocked){
+                if(ClueManager.instance.isPaintingUnlocked && currentHintIndex < 10){
                     currentHintIndex = 10;
+                } else {
+                    if(!ClueManager.instance.isPaintingUnlocked && currentHintIndex > 9){
+                        currentHintIndex = 9;
+                        hintsUsed--;
+                    }
                 }
 
-                hintText.text = hints[currentHintIndex];
+                if(ClueManager.instance.isDrawerUnlocked && currentHintIndex < 11){
+                    currentHintIndex = 11;
+                } else {
+                    if(!ClueManager.instance.isDrawerUnlocked && currentHintIndex > 10){
+                        currentHintIndex = 10;
+                        hintsUsed--;
+                    }
+                }
+
+                
                 currentHintIndex++;
-                hintsUsed++;
+                if(currentHintIndex == hints.Count){
+                    hintText.text = "Where are you?";
+                    currentHintIndex--;
+                }else{
+                    hintText.text = hints[currentHintIndex];
+                    hintsUsed++;
+                }
+                
             }
             else
             {
                 hintText.text = "Wait a bit!";
+                StartCoroutine(ResetHint(hints[currentHintIndex], 2f));
             }
         }
         else
         {
             hintText.text = "Where are you?";
         }
+        Debug.Log(currentHintIndex + " " + hintsUsed + " " + allowedHints);
     }
 
     public void SetHintIndex(int index)
     {
         currentHintIndex = index;
+    }
+
+    private IEnumerator ResetHint(string text, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        hintText.text = text;
     }
 }
